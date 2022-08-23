68a9f798a5ac84ab9ad74331e5b2bd70c75b5b02
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-08-22 18:56:12 -0600

    Add footer with links to GitHub & personal website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Footer from "./Footer";
 
 function App() {
-  return <div className="App">Hello from App!</div>;
+  return (
+    <div className="App">
+      Hello from App!
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <a
+          href="https://github.com/AddyLopez/shecodes-react-dictionary-app"
+          title="To Addy's GitHub repository for this project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://ubiquitous-froyo-69b6d7.netlify.app/index.html"
+          title="To Addy's personal website"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Addy López
+        </a>
+      </footer>
+    </div>
+  );
+}
