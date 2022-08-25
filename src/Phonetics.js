cfcae82931840ce0b9cb4f1264a19ec18ce5ca56
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-08-25 17:09:28 -0600

    Add play icon and audio functionality
    The Phonetics component uses the audio library Howler.js to play the phonetics audio file one time when the play icon is clicked.

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { Howl } from "howler";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import "./styles/Phonetics.css";
 
 export default function Phonetics(props) {
   console.log(props.phonetics);
   if (props.phonetics.audio.length > 0) {
+    const sound = new Howl({
+      src: [props.phonetics.audio],
+      html5: true,
+      loop: false,
+      preload: true,
+      volume: 1.0,
+    });
+
     return (
       <div className="Phonetics">
-        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-          Listen
-        </a>{" "}
+        <FontAwesomeIcon
+          icon={faCirclePlay}
+          className="play-icon"
+          type="button"
+          onClick={() => {
+            sound.play();
+            sound.once("end", function () {
+              sound.pause();
+            });
+          }}
+        />{" "}
         {props.phonetics.text}
       </div>
     );
