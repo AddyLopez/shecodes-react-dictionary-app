0b55c4c00ceedc28b16a4e924b46d66b7be36c5c
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-08-30 16:38:28 -0600

    Update default word

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import "./styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App defaultSearchWord="play" />
+    <App defaultSearchWord="stretch" />
   </React.StrictMode>
 );
